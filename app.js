374ef1683ecc0ce7a4af03d1097aca3c6f1a7dae
Takahiro Kato <dozo13189@gmail.com>
2020-09-27 23:13:21 +0900

    cant give args

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ function createApp(args) {
 
     app.publicCtx = new Proxy(ctx, {
         get(target, key, receiver) {
-            // console.log('get')
+            // console.log('get', target, key)
             track(target, key)
             return target[key]
         },
         set(target, key, value, receiver) {
+            // console.log('set', target, key, value, receiver)
             target[key] = value
             trigger(target, key)
-            console.log('set', target, key, value, receiver)
             return true
 
             // const res = target[key] = value
@@ -33,6 +33,7 @@ function createApp(args) {
         
         app.vnode = createVNode()
         effect(() => {
+            // console.log('re rendering')
             const nextVNode = render.call(app.publicCtx)
             patch(app.vnode, nextVNode, container)
             app.vnode = nextVNode
